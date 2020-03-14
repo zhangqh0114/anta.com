d38ba2cc780e0363e6b584d08b8362177a197e49
zack <346015525@qq.com>
2020-03-14 18:09:15 +0800

    done

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const connect = require('gulp-connect');
 const sourcemaps = require("gulp-sourcemaps");
-const babel = require("gulp-babel");  
-
+const babel = require("gulp-babel");
+const htmlmin = require('gulp-htmlmin');
 //搭建服务器环境
 gulp.task("server", done => {
     connect.server({
@@ -16,15 +16,16 @@ gulp.task("server", done => {
 //拷贝html文件
 gulp.task("html", done => {
     gulp.src('src/html/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist/html'))
         .pipe(connect.reload());//修改html后浏览器自动刷新
     done();
 });
 
 // 拷贝IMG文件
-gulp.task("imgs",done=>{
+gulp.task("imgs", done => {
     gulp.src('src/img/*.{jpg,png}')
-    .pipe(gulp.dest('dist/img'));
+        .pipe(gulp.dest('dist/img'));
     done();
 });
 //scss转css
@@ -38,18 +39,18 @@ gulp.task("sass", done => {
     done();
 });
 //拷贝libs 外部插件文件
-gulp.task("libs",done=>{
+gulp.task("libs", done => {
     gulp.src('src/libs/*.js')
-    .pipe(gulp.dest('dist/libs'))
-    ;
+        .pipe(gulp.dest('dist/libs'))
+        ;
     done();
 });
 //拷贝js文件
-gulp.task("babel",done=>{
+gulp.task("babel", done => {
     gulp.src('src/js/*.js')
-    .pipe(gulp.dest('dist/js'))
-    .pipe(babel({ presets: ["@babel/env"] }))
-    .pipe(connect.reload());
+        .pipe(gulp.dest('dist/js'))
+        .pipe(babel({ presets: ["@babel/env"] }))
+        .pipe(connect.reload());
     done();
 });
 //监听事件
